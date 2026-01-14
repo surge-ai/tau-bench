ad545030a081df5fc60342cb272c4ae3aa9a38e5
kmlefran-surge <kevinlefrancois@surgehq.ai>
2026-01-14 11:54:33 -0500

    Initial create order test

--- tau_bench/envs/worldbench_corecraft_computers/variations/variation_1/tools/__init__.py
+++ tau_bench/envs/worldbench_corecraft_computers/variations/variation_1/tools/__init__.py
@@ -1,6 +1,7 @@
 from .tau_calculate_price import CalculatePrice
 from .tau_check_warranty_status import CheckWarrantyStatus
 from .tau_create_escalation import CreateEscalation
+from .tau_create_order import CreateOrder
 from .tau_create_refund import CreateRefund
 from .tau_create_resolution import CreateResolution
 from .tau_get_customer_ticket_history import GetCustomerTicketHistory
@@ -24,6 +25,7 @@ ALL_TOOLS = [
     CalculatePrice,
     CheckWarrantyStatus,
     CreateEscalation,
+    CreateOrder,
     CreateRefund,
     CreateResolution,
     GetCustomerTicketHistory,
